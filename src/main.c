0ba741625c1a71528e6bcdc18183a03f7e4cc147
tgrid <mmazer9@gmail.com>
2017-01-11 02:05:41 +0700

    Added message receiving pthread

--- src/main.c
+++ src/main.c
@@ -6,17 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <libclipboard.h>
+#include <pthread.h>
 
 #ifdef _WIN32
 	#include <winsock.h>
 #else
 	#include <sys/socket.h>
 	#include <arpa/inet.h>
-	#include <unistd.h> /* Needed for close() */
+	#include <unistd.h>
 
 	typedef int SOCKET;
 #endif
+
+#include "libclipboard.h"
 
 int sock_init()
 {
@@ -39,57 +41,126 @@ int sock_quit()
 
 void usage()
 {
-	fprintf(stderr, "Usage: netclipboard server_address port [client_address]\n");
+	fprintf(stderr, "Usage: netclipboard server_address port\n");
 	exit(0);
 }
 
-#define SIZE 512
+#define BUFFER_SIZE 4096
+
+SOCKET sd;							/* Socket descriptor of server */
+unsigned short port_number;			/* Port number to use */
+int a1, a2, a3, a4;					/* Components of address in xxx.xxx.xxx.xxx form */
+byte stop = 0;
+
+struct sockaddr_in server;			/* Information about the server */
+struct sockaddr_in client;			/* Information about the client */
+struct hostent *hp;					/* Information about this computer */
+char host_name[256];				/* Name of the server */
+
+void *receiver_thread_func(){
+	fd_set readfds;
+	SOCKET n;
+	char buffer[BUFFER_SIZE];
+	int client_length;
+	int res;
+	struct timeval timeout;
+	timeout.tv_sec = 10;
+	timeout.tv_usec = 0;
+
+	FD_ZERO(&readfds);
+	FD_SET(sd, &readfds);
+
+	client_length = (int)sizeof(struct sockaddr_in);
+
+	n = sd + 1;
+
+	while (!stop)
+	{
+		res = select(n, &readfds, NULL, NULL, &timeout);
+		if (res == -1) {
+		    perror("select");
+		} else if (res == 0) {
+		    // printf("Timeout occurred!  No data after 10 seconds.\n");
+			/* Continue silently */
+		} else {
+		    if (FD_ISSET(sd, &readfds)) {
+		    	printf("Receiving...\n");
+				/* Receive bytes from client */
+				res = recvfrom(sd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client, &client_length);
+				if (res < 0)
+				{
+					fprintf(stderr, "Could not receive datagram.\n");
+				}
+				printf("Message received: '%s'\n", buffer);
+		    }
+
+		}
+	}
+	return NULL;
+}
+
+static int get_line(char *prmpt, char *buff, size_t sz) {
+    int ch, extra;
+
+    if (prmpt != NULL) {
+        printf ("%s", prmpt);
+        fflush (stdout);
+    }
+    if (fgets (buff, sz, stdin) == NULL)
+        return -1;
+
+    if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? -2 : 0;
+    }
+
+    buff[strlen(buff)-1] = '\0';
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-	unsigned int port_number;				/* The port number to use */
-	SOCKET sd;								/* The socket descriptor */
-	int server_length;						/* Length of server struct */
-	char send_buffer[SIZE] = "GET TIME\r\n";/* Data to send */
-	time_t current_time;					/* Time received */
-	struct hostent *hp;						/* Information about the server */
-	struct sockaddr_in server;				/* Information about the server */
-	struct sockaddr_in client;				/* Information about the client */
-	int a1, a2, a3, a4;						/* Server address components in xxx.xxx.xxx.xxx form */
-	int b1, b2, b3, b4;						/* Client address components in xxx.xxx.xxx.xxx form */
-	char host_name[256];					/* Host name of this computer */
-
-	if (argc != 3 && argc != 4)
+	/* Interpret command line */
+	if (argc == 2)
 	{
-		usage();
-	}
-	if (sscanf(argv[1], "%d.%d.%d.%d", &a1, &a2, &a3, &a4) != 4)
-	{
-		usage();
-	}
-	if (sscanf(argv[2], "%u", &port_number) != 1)
-	{
-		usage();
-	}
-	if (argc == 4)
-	{
-		if (sscanf(argv[3], "%d.%d.%d.%d", &b1, &b2, &b3, &b4) != 4)
+		/* Use local address */
+		if (sscanf(argv[1], "%u", &port_number) != 1)
 		{
 			usage();
 		}
 	}
+	else if (argc == 3)
+	{
+		/* Copy address */
+		if (sscanf(argv[1], "%d.%d.%d.%d", &a1, &a2, &a3, &a4) != 4)
+		{
+			usage();
+		}
+		if (sscanf(argv[2], "%u", &port_number) != 1)
+		{
+			usage();
+		}
+	}
+	else
+	{
+		usage();
+	}
 
+	/* Open windows connection */
 	if (!sock_init())
 	{
 		fprintf(stderr, "Could not open Windows connection.\n");
-		return 0;
+		exit(0);
 	}
 
+	/* Open a datagram socket */
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sd == INVALID_SOCKET)
 	{
 		fprintf(stderr, "Could not create socket.\n");
 		sock_quit();
-		return 0;
+		exit(0);
 	}
 
 	/* Clear out server struct */
@@ -99,20 +170,8 @@ int main(int argc, char *argv[]) {
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port_number);
 
-	/* Set server address */
-	server.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)a1;
-	server.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)a2;
-	server.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)a3;
-	server.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)a4;
-
-	/* Clear out client struct */
-	memset((void *)&client, '\0', sizeof(struct sockaddr_in));
-
-	/* Set family and port */
-	client.sin_family = AF_INET;
-	client.sin_port = htons(0);
-
-	if (argc == 3)
+	/* Set address automatically if desired */
+	if (argc == 2)
 	{
 		/* Get host name of this computer */
 		gethostname(host_name, sizeof(host_name));
@@ -124,47 +183,69 @@ int main(int argc, char *argv[]) {
 			fprintf(stderr, "Could not get host name.\n");
 			closesocket(sd);
 			sock_quit();
-			return 0;
+			exit(0);
 		}
 
 		/* Assign the address */
-		client.sin_addr.S_un.S_un_b.s_b1 = hp->h_addr_list[0][0];
-		client.sin_addr.S_un.S_un_b.s_b2 = hp->h_addr_list[0][1];
-		client.sin_addr.S_un.S_un_b.s_b3 = hp->h_addr_list[0][2];
-		client.sin_addr.S_un.S_un_b.s_b4 = hp->h_addr_list[0][3];
+		server.sin_addr.S_un.S_un_b.s_b1 = hp->h_addr_list[0][0];
+		server.sin_addr.S_un.S_un_b.s_b2 = hp->h_addr_list[0][1];
+		server.sin_addr.S_un.S_un_b.s_b3 = hp->h_addr_list[0][2];
+		server.sin_addr.S_un.S_un_b.s_b4 = hp->h_addr_list[0][3];
 	}
+	/* Otherwise assign it manually */
 	else
 	{
-		client.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)b1;
-		client.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)b2;
-		client.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)b3;
-		client.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)b4;
+		server.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)a1;
+		server.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)a2;
+		server.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)a3;
+		server.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)a4;
 	}
 
-	/* Bind local address to socket */
-	if (bind(sd, (struct sockaddr *)&client, sizeof(struct sockaddr_in)) == -1)
+	/* Bind address to socket */
+	if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1)
 	{
-		fprintf(stderr, "Cannot bind address to socket.\n");
+		fprintf(stderr, "Could not bind name to socket.\n");
 		closesocket(sd);
 		sock_quit();
+		exit(0);
+	}
+
+	/* Print out server information */
+	printf("Server running on %u.%u.%u.%u:%u\n", (unsigned char)server.sin_addr.S_un.S_un_b.s_b1,
+											  (unsigned char)server.sin_addr.S_un.S_un_b.s_b2,
+											  (unsigned char)server.sin_addr.S_un.S_un_b.s_b3,
+											  (unsigned char)server.sin_addr.S_un.S_un_b.s_b4,
+											  port_number);
+	printf("Type 'stop' to quit\n");
+
+	char input_string[10];
+	int res;
+	pthread_t receiver_thread;
+
+	if(pthread_create(&receiver_thread, NULL, receiver_thread_func, (void*)NULL))
+	{
+		fprintf(stderr, "Error creating thread\n");
+		return 0;
+	}
+
+	while (!stop)
+	{
+		res = get_line("> ", input_string, sizeof(input_string));
+		if (0 == res)
+		{
+			if (0 == strncmp("stop", input_string, strlen("stop")))
+				stop = 1;
+		}
+	}
+
+	if(pthread_join(receiver_thread, NULL))
+	{
+		fprintf(stderr, "Error joining thread\n");
 		return 0;
 	}
 
 	closesocket(sd);
 	sock_quit();
 
-    clipboard_c *cb = clipboard_new(NULL);
-    if (cb == NULL) {
-        printf("Clipboard initialisation failed!\n");
-        return 1;
-    }
-
-	char *text = clipboard_text_ex(cb, NULL, 0);
-	if (text != NULL) {
-		printf("%s\n", text);
-		free(text);
-	}
-
-    clipboard_free(cb);
     return 0;
 }
